fd8af654978b5fe3aeaf6c1f9e60cb2ffe643622
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-02 13:02:25 -0500

    finished part 1

--- Day2/part1.py
+++ Day2/part1.py
@@ -1,1 +1,24 @@
-print("hello world")
+input = open("input.txt", "r")
+lines = input.readlines()
+
+reports = []
+for i in range(len(lines)):
+    level = lines[i].split()
+    reports.append([int(level[i]) for i in range(len(level))])
+
+safe = 0
+for i in range(len(reports)):
+    level = reports[i]
+    isSafe = True
+    increasing = level[0] < level[1]
+    for j in range(len(level)-1):
+        isSafe = isSafe and abs(level[j+1]-level[j]) <=3
+        if isSafe:
+            if increasing:
+                isSafe = level[j] < level[j+1]
+            else:
+                isSafe = level[j] > level[j+1]
+    if isSafe:
+        safe +=1
+
+print(safe)
