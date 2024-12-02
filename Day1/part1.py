431ef2b7b538fce043fd02770a58cb23c8c5a6bf
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-01 20:21:45 -0500

    solved part 1

--- Day1/part1.py
+++ Day1/part1.py
@@ -1,1 +1,19 @@
+import numpy as np
+input = open("input.txt","r")
+lines = input.readlines()
 
+left = []
+right = []
+for i in range(len(lines)):
+    nums = lines[i].split()
+    left.append(int(nums[0]))
+    right.append(int(nums[1]))
+
+sum = 0
+
+for i in range(len(left)):
+    lmin = np.argmin(left)
+    rmin = np.argmin(right)
+    sum += abs(left.pop(lmin)-right.pop(rmin))
+
+print(sum)
