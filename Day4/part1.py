7c04bd9998447431261023fdbb03ac079c78bf9f
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-04 10:08:46 -0600

    I solved a different problem lol

--- Day4/part1.py
+++ Day4/part1.py
@@ -5,11 +5,10 @@ xmas = 0
 
 letters = {0:"X",1:"M",2:"A",3:"S"}
 def findXmas (row, col, letter):
-    if letter == 4:
-        return 1/8
-    if row < 0 or col < 0  or row >= len(grid) or col >= len(grid[0]) or grid[row][col] != letters[letter]:
+    if row < 0 or col < 0  or row >= len(grid) or col >= len(grid[0]):
         return 0
-    else:
+
+    if letter < 3 and grid[row][col] == letters[letter]:
         return findXmas(row+1,col, letter+1) + \
            findXmas(row-1,col, letter+1) + \
            findXmas(row,col+1, letter+1) + \
@@ -18,9 +17,10 @@ def findXmas (row, col, letter):
            findXmas(row-1,col-1, letter+1) +\
            findXmas(row+1,col-1, letter+1) +\
            findXmas(row-1,col+1, letter+1)
-
-
-
+    elif letter == 3 and grid[row][col] == "S":
+        return 1
+    else:
+        return 0
 
 for i in range(len(grid)):
     for j in range(len(grid[i])):
