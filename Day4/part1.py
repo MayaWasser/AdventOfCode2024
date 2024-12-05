86000fd668e4150b69cab43dbdd3c678926e668c
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-04 20:27:36 -0600

    solved part 1

--- Day4/part1.py
+++ Day4/part1.py
@@ -1,28 +1,44 @@
 input = open("input.txt","r")
-grid = input.readlines()
+Hgrid = input.readlines()
+
+m = len(Hgrid)
+n = len(Hgrid[0])
+
+Vgrid = []
+for i in range(n):
+    col = ""
+    for j in range(m):
+        col += Hgrid[j][i]
+    Vgrid.append(col)
+
+D1grid = []
+
+for i in range(m):
+    ud = ""
+    ld = ""
+    for j in range(m-i):
+        ld += Hgrid[i+j][j]
+        ud += Hgrid[j][i+j]
+    if i != 0:
+        D1grid.append(ud)
+    D1grid.append(ld)
+
+D2grid = []
+for i in range(m):
+    ud = ""
+    ld = ""
+    for j in range(i+1):
+        ud += Hgrid[i-j][j]
+        ld += Hgrid[m-1-j][m-1-i+j]
+    if i != m:
+        D2grid.append(ud)
+    D2grid.append(ld)
 
 xmas = 0
-
-letters = {0:"X",1:"M",2:"A",3:"S"}
-def findXmas (row, col, letter):
-    if row < 0 or col < 0  or row >= len(grid) or col >= len(grid[0]):
-        return 0
-
-    if letter < 3 and grid[row][col] == letters[letter]:
-        return findXmas(row+1,col, letter+1) + \
-           findXmas(row-1,col, letter+1) + \
-           findXmas(row,col+1, letter+1) + \
-           findXmas(row,col-1, letter+1) + \
-           findXmas(row+1,col+1, letter+1) +\
-           findXmas(row-1,col-1, letter+1) +\
-           findXmas(row+1,col-1, letter+1) +\
-           findXmas(row-1,col+1, letter+1)
-    elif letter == 3 and grid[row][col] == "S":
-        return 1
-    else:
-        return 0
-
-for i in range(len(grid)):
-    for j in range(len(grid[i])):
-        xmas += findXmas(i,j,0)
+for i in range(2*m-1):
+    if i < m:
+        xmas += Hgrid[i].count("XMAS") + Hgrid[i][::-1].count("XMAS")\
+            + Vgrid[i].count("XMAS") + Vgrid[i][::-1].count("XMAS")
+    xmas += D1grid[i].count("XMAS") + D1grid[i][::-1].count("XMAS")\
+        + D2grid[i].count("XMAS") + D2grid[i][::-1].count("XMAS")
 print(xmas)
