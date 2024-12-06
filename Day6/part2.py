795f417107816805661496c2bc0e10cddcadbd13
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-06 11:46:18 -0600

    this one's gonna take a while

--- Day6/part2.py
+++ Day6/part2.py
@@ -1,3 +1,4 @@
+import copy
 input = open("input.txt", "r").readlines()
 Os = ["O" for i in range(len(input[0])+1)]
 grid = [Os]
@@ -22,21 +23,23 @@ for i in range(1,m+1):
     for j in range(1,n+2):
         if grid[i][j] == ".":
             # test grid
-            test = grid.copy()
+            test = copy.deepcopy(grid)
             test[i][j] = "#"
             # guard coordinates and direction
-            gi = initi-1
+            gi = initi
             gj = initj
             dir = [-1,0]
-            while test[gi][gj]!= "O" or (gi==initi and gj ==initj and dir==[-1,0]):
+            step = 0
+            while test[gi][gj]!= "O" and not(gi==initi and gj==initj and dir==[-1,0] and step!=0):
                 if test[gi+dir[0]][gj+dir[1]] == "#":
                     dir = [dir[1], -dir[0]]
                 test[gi][gj] = "X"
                 gi += dir[0]
                 gj += dir[1]
+                step += 1
             if test[gi][gj] != "O":
                 count += 1
-            for k in range(len(test)):
-                print(test[k])
-            print("-------------------------------")
+
 print(count)
+# for i in range(len(grid)):
+#     print(grid[i])
