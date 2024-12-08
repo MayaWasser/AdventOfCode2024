cc25df13c3ff67e47e921c65a0cf7414b5cbca74
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-08 10:32:14 -0600

    finished part 1. Went smoorthly? I'm not used to that...

--- Day8/part1.py
+++ Day8/part1.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 input = open("input.txt","r").readlines()
 m = len(input)
 n = len(input[0])-1
@@ -13,4 +14,21 @@ for i in range(m):
             freqs[char].append((i,j))
 
 for lst in freqs.values():
-    print(lst)
+    for comb in combinations(lst,2):
+        di = comb[0][0]-comb[1][0]
+        dj = comb[0][1]-comb[1][1]
+        i1 = comb[0][0]+di
+        i2 = comb[1][0]-di
+        j1 = comb[0][1]+dj
+        j2 = comb[1][1]-dj
+        if i1 >= 0 and i1 < m and j1 >= 0  and j1 < n:
+            anodes[i1][j1] = "#"
+        if i2 >= 0 and i2 < m and j2 >= 0  and j2 < n:
+            anodes[i2][j2] = "#"
+
+count = 0
+for row in anodes:
+    for char in row:
+        if char == "#":
+            count+=1
+print(count)
