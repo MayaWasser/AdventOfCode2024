eee7430186964e23c71a31a79274257289d5fd39
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-10 18:14:53 -0600

    working with sets now I guess

--- Day6/part2.py
+++ Day6/part2.py
@@ -1,4 +1,5 @@
 import copy
+
 input = open("input.txt", "r").readlines()
 Os = ["O" for i in range(len(input[0])+1)]
 grid = [Os]
@@ -20,7 +21,7 @@ for i in range(1,m+1):
 
 count = 0
 for i in range(1,m+1):
-    for j in range(1,n+2):
+    for j in range(1,n+1):
         if grid[i][j] == ".":
             # test grid
             test = copy.deepcopy(grid)
@@ -29,18 +30,48 @@ for i in range(1,m+1):
             gi = initi
             gj = initj
             dir = [-1,0]
-            step = 0
-            while test[gi][gj]!= "O" and not(gi==initi and gj==initj and dir==[-1,0] and step!=0):
-                if test[gi+dir[0]][gj+dir[1]] == "#":
-                    dir = [dir[1], -dir[0]]
-                test[gi][gj] = "X"
-                gi += dir[0]
-                gj += dir[1]
-                step += 1
-            if test[gi][gj] != "O":
-                count += 1
+            visited = set()
+            # while grid[gi][gj] != "O" and ((gi,gj,dir) not in visited):
+            #     visited.add((gi,gj,dir))
+            #     if grid[gi+dir[0]][gj+dir[1]] == "#":
+            #         dir = [dir[1], -dir[0]]
+            #     gi += dir[0]
+            #     gj += dir[1]
+            # if grid[gi][gj] != "O":
+            #     count += 1
+set 
+# for i in range(1,m+1):
+#     for j in range(1,n+1):
+#         if grid[i][j] == "^":
+#             # guard row
+#             initi = i
+#             # guard column
+#             initj = j
+#
+# count = 0
+# for i in range(1,m+1):
+#     for j in range(1,n+2):
+#         if grid[i][j] == ".":
+#             # test grid
+#             test = copy.deepcopy(grid)
+#             test[i][j] = "#"
+#             # guard coordinates and direction
+#             gi = initi
+#             gj = initj
+#             dir = [-1,0]
+#             step = 0
+#             while test[gi][gj]!= "O" and not(gi==initi and gj==initj and dir==[-1,0] and step!=0):
+#                 if test[gi+dir[0]][gj+dir[1]] == "#":
+#                     dir = [dir[1], -dir[0]]
+#                 test[gi][gj] = "X"
+#                 gi += dir[0]
+#                 gj += dir[1]
+#                 step += 1
+#             if test[gi][gj] != "O":
+#                 count += 1
+#
+# print(count)
 
-print(count)
 # for i in range(len(grid)):
 #     print(grid[i])
 
