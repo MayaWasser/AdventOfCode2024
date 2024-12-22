4cdd50cc7ae93f038cadc1871e49917eea1a3413
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-22 17:29:51 -0500

    prints all possible paths

--- Day16/part1.py
+++ Day16/part1.py
@@ -10,66 +10,31 @@ for i in range(len(maze)):
         if maze[i][j] == "S":
             srow = i
             scol = j
+            maze[i][j] = "."
 
 
-# direction of previous move
-# -1 on the first move
-# up = 1
-# down = 2
-# left = 3
-# right = 4
-
-def solve(i, j, dir,score,lst):
+def solve(i,j):
     char = maze[i][j]
-    if char == "#" or char == "@" or char == "*":
-        return -1
     if char == "E":
-        lst.append(score)
-        print(score)
-        score = 0
-        return 0
+        printMaze()
 
-    maze[i][j] = "@"
-    up = solve(i-1,j,1,score,lst)
-    if up != -1:
-        if dir == 1 or dir == -1:
-            score += 1
-        else:
-            score += 1000
+    if char == ".":
+        maze[i][j] = "@"
 
-    down = solve(i+1,j,2,score,lst)
-    if down != -1 :
-        if dir == 2  or dir == -1:
-            score += 1
-        else:
-            score += 1000
+        up = solve(i-1,j)
+        down = solve(i+1,j)
+        left = solve(i,j-1)
+        right = solve(i,j+1)
 
-    left = solve(i,j-1,3,score,lst)
-    if left != -1 :
-        if dir == 3  or dir == -1:
-            score += 1
-        else:
-            score += 1000
+        # visited but not part of path
+        maze[i][j] = "."
 
-    right = solve(i,j+1,4,score,lst)
-    if right != -1 :
-        if dir == 4  or dir == -1:
-            score += 1
-        else:
-            score += 1000
 
-    # visited but not part of path
-    maze[i][j] = "*"
-
-    return -1
-
-lst = []
-score = 0
-dir = -1
-print( solve(srow,scol,dir,score,lst) )
-print(lst)
-
-for row in maze:
-    for char in row:
-        print(char, end="")
+def printMaze():
+    for row in maze:
+        for char in row:
+            print(char, end="")
+        print()
     print()
+
+solve(srow,scol)
