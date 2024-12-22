4d6d8fd7957d1a778c6a007072a99a2ba30d0787
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-22 17:06:58 -0500

    have something (?) but I'm gonna pivot to a different solution

--- Day16/part1.py
+++ Day16/part1.py
@@ -12,36 +12,62 @@ for i in range(len(maze)):
             scol = j
 
 
-def solve(i, j):
+# direction of previous move
+# -1 on the first move
+# up = 1
+# down = 2
+# left = 3
+# right = 4
+
+def solve(i, j, dir,score,lst):
     char = maze[i][j]
     if char == "#" or char == "@" or char == "*":
         return -1
     if char == "E":
+        lst.append(score)
+        print(score)
+        score = 0
         return 0
 
     maze[i][j] = "@"
-    up = solve(i-1,j)
+    up = solve(i-1,j,1,score,lst)
     if up != -1:
-        return 1+up
+        if dir == 1 or dir == -1:
+            score += 1
+        else:
+            score += 1000
 
-    down = solve(i+1,j)
+    down = solve(i+1,j,2,score,lst)
     if down != -1 :
-        return 1+down
+        if dir == 2  or dir == -1:
+            score += 1
+        else:
+            score += 1000
 
-    left = solve(i,j-1)
+    left = solve(i,j-1,3,score,lst)
     if left != -1 :
-        return 1+left
+        if dir == 3  or dir == -1:
+            score += 1
+        else:
+            score += 1000
 
-    right = solve(i,j+1)
+    right = solve(i,j+1,4,score,lst)
     if right != -1 :
-        return 1+right
+        if dir == 4  or dir == -1:
+            score += 1
+        else:
+            score += 1000
 
     # visited but not part of path
     maze[i][j] = "*"
-    
+
     return -1
 
-solve(srow,scol)
+lst = []
+score = 0
+dir = -1
+print( solve(srow,scol,dir,score,lst) )
+print(lst)
 
 for row in maze:
     for char in row:
