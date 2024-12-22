161f2294e36127ef176fffe130ada854de0b3535
Maya Wassercug <mwassercug40@stuy.edu>
2024-12-22 17:52:10 -0500

    works for examples but not input HAAAAAAAA

--- Day16/part1.py
+++ Day16/part1.py
@@ -4,6 +4,7 @@ maze = [ list(input[i].rstrip()) for i in range(len(input)) ]
 # starting position
 srow = -1
 scol = -1
+scores = []
 
 for i in range(len(maze)):
     for j in range(len(maze[i])):
@@ -12,21 +13,43 @@ for i in range(len(maze)):
             scol = j
             maze[i][j] = "."
 
+# direction of the previous move (-1 on the first)
+# 1 = up
+# 2 = down
+# 3 = left
+# 4 = right
 
-def solve(i,j):
+def solve(i,j, dir, score):
     char = maze[i][j]
     if char == "E":
-        printMaze()
+        scores.append(score)
+        # print(score)
+        # printMaze()
 
     if char == ".":
         maze[i][j] = "@"
 
-        up = solve(i-1,j)
-        down = solve(i+1,j)
-        left = solve(i,j-1)
-        right = solve(i,j+1)
+        if dir == 1:
+            solve(i-1,j,1,score+1)
+        else:
+            solve(i-1,j,1,score+1001)
 
-        # visited but not part of path
+        if dir == 2:
+            solve(i+1,j,2,score+1)
+        else:
+            solve(i+1,j,2,score+1001)
+
+        if dir == 3:
+            solve(i,j-1,3,score+1)
+        else:
+            solve(i,j-1,3,score+1001)
+
+        if dir == 4:
+            solve(i,j+1,4,score+1)
+        else:
+            solve(i,j+1,4,score+1001)
+
+
         maze[i][j] = "."
 
 
@@ -37,4 +60,6 @@ def printMaze():
         print()
     print()
 
-solve(srow,scol)
+
+solve(srow,scol, -1, 0)
+print(min(scores))
